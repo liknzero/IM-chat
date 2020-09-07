224bce47b13e4f99f5be685f74bf360df992860e
unknown <mingzhao233@163.com>
2020-09-07 21:32:05 +0800

    支付组件

---
+++ componentVue/pay/choosePayment.vue
@@ -0,0 +1,67 @@
+<template>
+    <van-action-sheet
+            v-model="showPay"
+            safe-area-inset-bottom
+            cancel-text="取消"
+            close-on-click-action
+            @cancel="onCancel"
+    >
+        <div class="radio-title">选择支付方式</div>
+        <van-radio-group v-model="radio">
+            <van-cell-group>
+                <van-cell
+                        v-for="item in actionPay"
+                        clickable
+                        @click="radio = item.pay"
+                >
+                    <template #icon>
+                        <van-icon :name="item.icon" />
+                    </template>
+                    <template #title>
+                        <div>{{item.name}}</div>
+                    </template>
+                    <template #right-icon>
+                        <van-radio checked-color="#F078A2" :name="item.pay" />
+                    </template>
+                </van-cell>
+            </van-cell-group>
+        </van-radio-group>
+    </van-action-sheet>
+</template>
+
+<script>
+    // TODO: 这里要做支付状态的筛选和选择状态的监听
+    export default {
+        name: "choosePayment",
+        data () {
+            return {
+                actionPay: [
+                    {
+                        pay: 2,
+                        name: '微信支付(推荐）',
+                        icon: '/waps/images/icon-pay-wechat.png'
+                    },
+                    {
+                        pay: 3,
+                        name: '支付宝支付',
+                        icon: '/waps/images/icon-pay-ali.png'
+                    },
+                ]
+            }
+        },
+        props: {
+            radio: {
+                type: Number,
+                default: 0
+            },
+            showPay: {
+                type: Boolean,
+                default: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ componentVue/pay/orderList.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="order-list">
+        <div class="order-list-title">跨境知道</div>
+        <div
+                class="collection-cell"
+                @click="linkTo(item)"
+        >
+            <img
+                    class="order-cover"
+                    :src="'/waps/images/icon-default-avatar.png'"
+                    mode="aspectFill"
+                    alt=""
+            >
+            <div class="collec-cell-rt">
+                <div class="cell-rt-title">{{ '啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方' }}</div>
+                <span class="cell-rt-num">
+                    <span>¥{{45894}}</span>&nbsp;
+                    <span class="cell-num-out">¥{{45894}}</span>
+                </span>
+            </div>
+        </div>
+        <div
+                class="collection-cell"
+                @click="linkTo(item)"
+        >
+            <img
+                    class="order-cover"
+                    :src="'/waps/images/icon-default-avatar.png'"
+                    mode="aspectFill"
+                    alt=""
+            >
+            <div class="collec-cell-rt">
+                <div class="cell-rt-title">{{ '啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方啊手动阀第三方' }}</div>
+                <span class="cell-rt-num">
+                    <span>¥{{45894}}</span>&nbsp;
+                    <span class="cell-num-out">¥{{45894}}</span>
+                </span>
+            </div>
+        </div>
+        <div class="order-all-num">小计：¥{{320.00}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "orderList",
+        methods: {
+            linkTo () {
+
+            }
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+@import "../../assets/commonCss/base";
+.order-list {
+    padding: 0 30px;
+    background: #ffffff;
+    margin-bottom: 20px;
+    .order-list-title {
+        height: 72px;
+        line-height: 72px;
+        font-size: 24px;
+        color: #909099;
+    }
+    .collection-cell {
+        padding: 28px 0;
+        position: relative;
+        display: flex;
+        &:after {
+            .border-bottom-1px(#EBEBEB);
+        }
+        .order-cover {
+            width: 200px;
+            height: 112px;
+            object-fit: cover;
+        }
+        .collec-cell-rt {
+            padding-left: 20px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            .cell-rt-title {
+                font-size: 28px;
+                font-weight: bold;
+                color: #303033;
+                .line-clamp(2);
+            }
+            .cell-rt-num {
+                font-size: 24px;
+                color: #2C2C33;
+                .cell-num-out {
+                    color: #A8A8B3;
+                    text-decoration:line-through
+                }
+            }
+        }
+    }
+    .order-all-num {
+        height: 88px;
+        line-height: 88px;
+        font-size: 28px;
+        font-weight: bold;
+        color: #2C2C33;
+        padding: 0 30px;
+        text-align: right;
+    }
+}
+</style>
